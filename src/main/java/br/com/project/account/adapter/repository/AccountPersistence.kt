308fbd6c7e34f05bc55029fcdd0e373fb18a1599
Adriano Marciao <adrmarciaopereira@gmail.com>
2022-01-30 14:39:07 -0400

    feat(docker): criando dockerfile para criar uma imagem do microservico

--- src/main/java/br/com/project/account/adapter/repository/AccountPersistence.kt
+++ src/main/java/br/com/project/account/adapter/repository/AccountPersistence.kt
@@ -4,7 +4,6 @@ import br.com.project.account.model.Account
 import br.com.project.account.ports.AccountPersistencePort
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Primary
-import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
